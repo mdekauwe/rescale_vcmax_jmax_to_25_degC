6847d2bffad4a67abd903dd247a779ad580ea99b
mdekauwe <mdekauwe@gmail.com>
2026-03-24 15:39:48 +0000

    added script

---
+++ rescale_vcmax_to_vcmax25.R
@@ -0,0 +1,16 @@
+
+# Constants
+RGAS <- 8.314   # J mol-1 K-1
+Eav <- 65300.0  # J mol-1 (Bernacchi et al. 2004 PCE)
+
+Tleaf_C <- 10.0
+VcmaxT <- 22.0
+
+# Convert to Kelvin
+Tleaf_K <- Tleaf_C + 273.15
+Tref_K <- 298.15  # 25 deg C
+
+Vcmax25 <- VcmaxT * exp(Eav * (Tref_K - Tleaf_K) / (RGAS * Tleaf_K * Tref_K))
+
+
+print(Vcmax25)
